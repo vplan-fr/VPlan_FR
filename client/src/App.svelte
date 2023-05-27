b92f288f0522bd1a3fc5846695ff6be37e173bcc
ArtrenH <wilhelm.ostwald@posteo.de>
2023-05-27 03:30:43 +0200

    first connection between back- and frontend

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,9 +1,34 @@
 <script>
 	export let name;
+    let lessons = [
+    ]
+    function load_lessons(school_num, date, plan_type, entity) {
+        fetch(`./api/v69.420/${school_num}/plan?date=${date}`)
+            .then(response => response.json())
+            .then(data => {
+                lessons = data["plans"][plan_type + "_plan"][entity];
+                console.log(lessons);
+            })
+            .catch(error => {
+                console.error(error);
+        });
+    }
+    load_lessons("10001329", "2023-05-22", "form", "JG12");
+    console.log(lessons)
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
+    {#each lessons as lesson}
+        <div class="card lesson-head">{lesson.begin}-{lesson.end} (#{lesson.period})</div>
+        <div class="card">{lesson.form}</div>
+        <div class="card">{lesson.current_subject}</div>
+        <div class="card">{lesson.current_teacher}</div>
+        {#if lesson.info}
+            <div class="card">{lesson.info}</div>
+        {/if}
+        <br>
+    {/each}
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps. <span class="material-symbols-outlined">settings</span></p>
     Lorem<br>ipsum<br>dolor<br>sit<br>amet<br>consectetur<br>adipisicing<br>elit.<br>Commodi<br>officia<br>natus<br>ad,<br>aut<br>accusantium<br>dolores<br>totam<br>veritatis<br>placeat<br>eligendi<br>repudiandae,<br>fugiat<br>facere<br>veniam<br>non<br>fugit<br>fuga<br>temporibus<br>optio<br>ex<br>deserunt.
     Lorem<br>ipsum<br>dolor<br>sit<br>amet<br>consectetur<br>adipisicing<br>elit.<br>Commodi<br>officia<br>natus<br>ad,<br>aut<br>accusantium<br>dolores<br>totam<br>veritatis<br>placeat<br>eligendi<br>repudiandae,<br>fugiat<br>facere<br>veniam<br>non<br>fugit<br>fuga<br>temporibus<br>optio<br>ex<br>deserunt.
@@ -15,6 +40,12 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+        .card {
+            color: black;
+            &.lesson-head {
+                color: red;
+            }
+        }
 	}
 
 	h1 {
--- server.py
+++ server.py
@@ -1,21 +1,63 @@
-from flask import Flask, send_from_directory
+import os
+import json
+from flask import Flask, send_from_directory, request, jsonify
 import random
+
+VALID_SCHOOLS = os.listdir(".cache")
+API_BASE_URL = "/api/v69.420/<school_num>"
 
 app = Flask(__name__)
 
-# Path for our main Svelte page
+# COMPILED SVELTE FILES
+
+
 @app.route("/")
 def base():
     return send_from_directory('client/public', 'index.html')
 
-# Path for all the static files (compiled JS/CSS, etc.)
+
 @app.route("/<path:path>")
 def home(path):
     return send_from_directory('client/public', path)
 
-@app.route("/rand")
-def hello():
-    return str(random.randint(0, 100))
+
+# PLAN JSON API
+
+@app.route(f"{API_BASE_URL}/dates")
+def dates(school_num):
+    if school_num not in VALID_SCHOOLS:
+        return {"error": "school not in database"}
+    with open(f".cache/{school_num}/meta.json", "r") as f:
+        return jsonify(json.load(f)["dates"])
+
+
+@app.route(f"{API_BASE_URL}/meta")
+def meta(school_num):
+    if school_num not in VALID_SCHOOLS:
+        return {"error": "school not in database"}
+    with open(f".cache/{school_num}/meta.json", "r") as f:
+        return jsonify(json.load(f))
+
+
+@app.route(f"{API_BASE_URL}/plan")
+def plan(school_num: str):
+    print("getting plan")
+    if school_num not in VALID_SCHOOLS:
+        return {"error": "school not in database"}
+    date = request.args.get("date")
+    if not date:
+        return {"error": "please specify a date"}
+    if date not in os.listdir(f".cache/{school_num}/plans"):
+        return {"error": "date not found"}
+    revision_stamp = request.args.get("revision", ".newest")
+    if revision_stamp not in os.listdir(f".cache/{school_num}/plans/{date}"):
+        return {"error": "revision not found"}
+    data = {}
+    for plan_type in ["plans", "rooms"]:
+        with open(f".cache/{school_num}/plans/{date}/{revision_stamp}/{plan_type}.json", "r") as f:
+            data[plan_type] = json.load(f)
+    return jsonify(data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
