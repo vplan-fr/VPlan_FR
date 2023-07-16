9c343bcb2ab4b47a6e7de62079f9d4db5abea76e
Belissimo_T <bela.tollking@gmail.com>
2023-07-16 21:26:40 +0200

    explicitly use utc timezone for cache

--- backend/cache.py
+++ backend/cache.py
@@ -14,7 +14,10 @@ class Cache:
         if timestamp is None:
             return self.path / "plans" / day.isoformat()
         elif isinstance(timestamp, datetime.datetime):
-            return self.path / "plans" / day.isoformat() / timestamp.strftime("%Y-%m-%dT%H-%M-%S")
+            return (
+                    self.path / "plans" / day.isoformat()
+                    / timestamp.astimezone(datetime.timezone.utc).strftime("%Y-%m-%dT%H-%M-%S")
+            )
         else:
             return self.path / "plans" / day.isoformat() / timestamp
 
@@ -27,14 +30,34 @@ class Cache:
         with open(path, "w", encoding="utf-8") as f:
             f.write(content)
 
-    def get_timestamps(self, day: datetime.date) -> list[datetime.datetime]:
-        """Return all stored timestamps for a given day."""
+    def get_plan_file(self,
+                      day: datetime.date,
+                      timestamp: datetime.datetime | typing.Literal[".newest"],
+                      filename: str) -> str:
+        """Return the contents of a plan file from the cache."""
+        # self._logger.debug(f"get_plan_file({day!r}, {timestamp!r}, {filename!r})")
 
-        path = self.get_plan_path(day, None)
-        return sorted([
-            datetime.datetime.strptime(elem.stem, "%Y-%m-%dT%H-%M-%S")
-            for elem in path.iterdir() if elem.is_dir() and not elem.stem.startswith(".")
-        ], reverse=True)
+        path = self.get_plan_path(day, timestamp) / filename
+
+        with open(path, "r", encoding="utf-8") as f:
+            return f.read()
+
+    def store_meta_file(self, content: str, filename: str):
+        """Store a meta file in the cache such as "meta.json"."""
+
+        path = self.path / filename
+        path.parent.mkdir(parents=True, exist_ok=True)
+
+        with open(path, "w", encoding="utf-8") as f:
+            f.write(content)
+
+    def get_meta_file(self, filename: str) -> str:
+        """Return the contents of a meta file from the cache."""
+
+        path = self.path / filename
+
+        with open(path, "r", encoding="utf-8") as f:
+            return f.read()
 
     def get_days(self, reverse=True) -> list[datetime.date]:
         """Return a list of all days for which plans are stored."""
@@ -48,6 +71,15 @@ class Cache:
             for elem in path.iterdir() if elem.is_dir()
         ], reverse=reverse)
 
+    def get_timestamps(self, day: datetime.date) -> list[datetime.datetime]:
+        """Return all stored timestamps for a given day."""
+
+        path = self.get_plan_path(day, None)
+        return sorted([
+            datetime.datetime.strptime(elem.stem, "%Y-%m-%dT%H-%M-%S").replace(tzinfo=datetime.timezone.utc)
+            for elem in path.iterdir() if elem.is_dir() and not elem.stem.startswith(".")
+        ], reverse=True)
+
     def set_newest(self, day: datetime.date, timestamp: datetime.datetime):
         newest_path = self.get_plan_path(day, ".newest")
         target_path = self.get_plan_path(day, timestamp)
@@ -60,17 +92,6 @@ class Cache:
         self.get_plan_path(day, ".newest").unlink(missing_ok=True)
         if timestamps:
             self.set_newest(day, timestamps[0])
-
-    def get_plan_file(self,
-                      day: datetime.date,
-                      timestamp: datetime.datetime | typing.Literal[".newest"],
-                      filename: str) -> str:
-        """Return the contents of a plan file from the cache."""
-
-        path = self.get_plan_path(day, timestamp) / filename
-
-        with open(path, "r", encoding="utf-8") as f:
-            return f.read()
 
     def get_all_json_plan_files(self,
                                 day: datetime.date,
@@ -89,20 +110,3 @@ class Cache:
                  timestamp: datetime.datetime | typing.Literal[".newest"],
                  filename: str) -> bool:
         return (self.get_plan_path(day, timestamp) / filename).exists()
-
-    def store_meta_file(self, content: str, filename: str):
-        """Store a meta file in the cache such as "meta.json"."""
-
-        path = self.path / filename
-        path.parent.mkdir(parents=True, exist_ok=True)
-
-        with open(path, "w", encoding="utf-8") as f:
-            f.write(content)
-
-    def get_meta_file(self, filename: str) -> str:
-        """Return the contents of a meta file from the cache."""
-
-        path = self.path / filename
-
-        with open(path, "r", encoding="utf-8") as f:
-            return f.read()
