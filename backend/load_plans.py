ad04682daa66fdca8aac7840e7c6d07ec530c027
bela <bela.tollking@gmail.com>
2023-07-23 13:08:26 +0200

    add try/except/else clause in compute_plans() for the case that no indiware form plan is available

--- backend/load_plans.py
+++ backend/load_plans.py
@@ -248,47 +248,56 @@ class PlanProcessor:
         return True
 
     def compute_plans(self, date: datetime.date, timestamp: datetime.datetime):
-        plan_kl = self.cache.get_plan_file(date, timestamp, "PlanKl.xml")
         try:
-            vplan_kl = self.cache.get_plan_file(date, timestamp, "VPlanKl.xml")
+            plan_kl = self.cache.get_plan_file(date, timestamp, "PlanKl.xml")
         except FileNotFoundError:
-            vplan_kl = None
-        plan_extractor = PlanExtractor(plan_kl, vplan_kl, self.teachers.abbreviation_by_surname(), logger=self._logger)
+            self._logger.warning(f"=> Could not find Indiware form plan for date {date!s} and timestamp {timestamp!s}.")
+        else:
+            try:
+                vplan_kl = self.cache.get_plan_file(date, timestamp, "VPlanKl.xml")
+            except FileNotFoundError:
+                vplan_kl = None
+                self._logger.debug(
+                    f"=> Could not find substitution plan for date {date!s} and timestamp {timestamp!s}."
+                )
 
-        self.cache.store_plan_file(
-            date, timestamp,
-            json.dumps({
-                "rooms": plan_extractor.room_plan(),
-                "teachers": plan_extractor.teacher_plan(),
-                "forms": plan_extractor.form_plan()
-            }, default=Lessons.serialize),
-            "plans.json"
-        )
+            plan_extractor = PlanExtractor(plan_kl, vplan_kl, self.teachers.abbreviation_by_surname(),
+                                           logger=self._logger)
 
-        self.cache.store_plan_file(
-            date, timestamp,
-            json.dumps(plan_extractor.plan.exams, default=Exam.to_dict),
-            "exams.json"
-        )
+            self.cache.store_plan_file(
+                date, timestamp,
+                json.dumps({
+                    "rooms": plan_extractor.room_plan(),
+                    "teachers": plan_extractor.teacher_plan(),
+                    "forms": plan_extractor.form_plan()
+                }, default=Lessons.serialize),
+                "plans.json"
+            )
 
-        all_rooms = self.meta_extractor.rooms()
-        rooms_data = {
-            "used_rooms_by_period": plan_extractor.used_rooms_by_period(),
-            "free_rooms_by_period": plan_extractor.free_rooms_by_period(all_rooms),
-            "free_rooms_by_block": plan_extractor.free_rooms_by_block(all_rooms)
-        }
+            self.cache.store_plan_file(
+                date, timestamp,
+                json.dumps(plan_extractor.plan.exams, default=Exam.to_dict),
+                "exams.json"
+            )
 
-        self.cache.store_plan_file(
-            date, timestamp,
-            json.dumps(rooms_data, default=list),
-            "rooms.json"
-        )
+            all_rooms = self.meta_extractor.rooms()
+            rooms_data = {
+                "used_rooms_by_period": plan_extractor.used_rooms_by_period(),
+                "free_rooms_by_period": plan_extractor.free_rooms_by_period(all_rooms),
+                "free_rooms_by_block": plan_extractor.free_rooms_by_block(all_rooms)
+            }
 
-        self.cache.store_plan_file(
-            date, timestamp,
-            json.dumps(plan_extractor.info_data()),
-            "info.json"
-        )
+            self.cache.store_plan_file(
+                date, timestamp,
+                json.dumps(rooms_data, default=list),
+                "rooms.json"
+            )
+
+            self.cache.store_plan_file(
+                date, timestamp,
+                json.dumps(plan_extractor.info_data()),
+                "info.json"
+            )
 
         self.cache.store_plan_file(date, timestamp, str(self.VERSION), ".processed")
 
