2f3725a785df0a194ecdc602a35b7d16ae47fe3e
ArtrenH <79058115+ArtrenH@users.noreply.github.com>
2023-09-11 23:39:42 +0200

    better preferences

--- client/src/Preferences.svelte
+++ client/src/Preferences.svelte
@@ -7,13 +7,12 @@
     export let api_base;
     export let grouped_forms;
     export let course_lists;
-    let selected_form;
+    let selected_form = null;
     let form_groups;
     $: form_groups = selected_form != null ? sortCourses(course_lists[selected_form]["class_groups"]): [];
 
     let allItems = [];
     let selection = {};
-    let selectedItems = [];
     let current_form_preferences = [];
 
 
@@ -25,22 +24,27 @@
         });
         return sortedObject;
     }
-    function toggleSelection(course_id) {
-        console.log(course_id);
-        if (!selectedItems.includes(course_id)) {
-            selectedItems.push(course_id)
-        } else {
-            selectedItems.splice(selectedItems.indexOf(course_id), 1)
+
+    function getFalse(obj) {
+        const falseItems = [];
+        for (const key in obj) {
+            if (obj.hasOwnProperty(key) && obj[key] === false) {
+                falseItems.push(key);
+            }
         }
-        console.log(selectedItems);
+        return falseItems;
     }
 
     function updateCourses() {
+        if (selected_form === null) {
+             return
+        }
         allItems = Object.keys(course_lists[selected_form]["class_groups"]);
-        selection = {};
-
         current_form_preferences = $preferences[selected_form] || [];
-        selectedItems = allItems.filter(item => current_form_preferences.indexOf(item) !== -1);
+        selection = {};
+        for (const item of allItems) {
+            selection[item] = !current_form_preferences.includes(item);
+        }
     }
 
 
@@ -61,27 +65,53 @@
             }
         ), {
             method: "POST",
-            body: JSON.stringify(selectedItems)
+            body: JSON.stringify(getFalse(selection))
         })
             .then(data => {
-                if (data["success"]) {
-                    notifications.info("Kurse gespeichert!", 2000);
-                }
+                notifications.info("Kurse gespeichert!", 2000);
             })
             .catch(error => {
                 notifications.danger(error);
             })
     }
 
-    getPreferences()
-    function isChecked(id) {
-        return selectedItems.includes(id)
+    getPreferences();
+    updateCourses();
+
+    function select_all() {
+        for (const key in selection) {
+            if (selection.hasOwnProperty(key)) {
+                selection[key] = true;
+            }
+        }
     }
+    function select_none() {
+        for (const key in selection) {
+            if (selection.hasOwnProperty(key)) {
+                selection[key] = false;
+            }
+        }
+    }
+    function reverse_selection() {
+        for (const key in selection) {
+            if (selection.hasOwnProperty(key)) {
+                selection[key] = !selection[key];
+            }
+        }
+    }
+
 
 </script>
 
 
-Klasse wählen: <select name="forms" bind:value={selected_form} on:change={updateCourses}>
+{#if selected_form != null}
+    <button on:click={select_all}>Alle auswählen</button>
+    <button on:click={select_none}>Nichts auswählen</button>
+    <button on:click={reverse_selection}>Auswahl invertieren</button>
+    <br>
+{/if}
+Klasse wählen:
+<select name="forms" bind:value={selected_form} on:change={updateCourses}>
     {#each Object.entries(grouped_forms) as [form_group, forms]}
         <optgroup label={form_group}>
             {#each forms as form}
@@ -97,18 +127,10 @@ Klasse wählen: <select name="forms" bind:value={selected_form} on:change={updat
     <ul>
         {#each Object.keys(form_groups) as course_id}
             <li>
-                {#if current_form_preferences.includes(course_id)}
-                    <input
-                        type="checkbox"
-                        on:change={() => toggleSelection(course_id)}
-                      />
-                {:else}
-                    <input
-                        type="checkbox"
-                        on:change={() => toggleSelection(course_id)}
-                        checked
-                    />
-                {/if}
+                <input
+                    type="checkbox"
+                    bind:checked={selection[course_id]}
+                />
                 {course_id}
                 {form_groups[course_id]["teacher"]} |
                 {form_groups[course_id]["subject"]}
