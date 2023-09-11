ff0ff73bd063cf53a39970d905d70bc4ae1981ac
Belissimo_T <bela.tollking@gmail.com>
2023-09-11 19:56:16 +0200

    remove redundant variable declaration

--- client/src/Lesson.svelte
+++ client/src/Lesson.svelte
@@ -8,7 +8,6 @@
     export let display_time = true;
 
     $: teacher_absent = lesson.scheduled_teachers?.length !== 0 && lesson.current_teachers.length === 0 && lesson.takes_place
-    $: subject = (lesson.takes_place ? lesson.current_class : lesson.scheduled_class)
     $: teachers = (lesson.takes_place && !teacher_absent ? lesson.current_teachers : lesson.scheduled_teachers) || []
     $: forms = (lesson.takes_place ? lesson.current_forms : lesson.scheduled_forms) || []
     $: forms_str = lesson.takes_place ? lesson.current_forms_str : lesson.scheduled_forms_str
