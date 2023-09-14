cd3797e3902d58ac399c8d00d5f544ea9b087437
Belissimo_T <bela.tollking@gmail.com>
2023-09-15 00:00:20 +0200

    fix #96

--- backend/plan_processor.py
+++ backend/plan_processor.py
@@ -13,7 +13,7 @@ from .vplan_utils import group_forms, ParsedForm
 
 
 class PlanProcessor:
-    VERSION = "52"
+    VERSION = "53"
 
     def __init__(self, cache: Cache, school_number: str, *, logger: logging.Logger):
         self._logger = logger
