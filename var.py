56bf423ed57402176edffd41f997f72914a61953
ArtrenH <79058115+ArtrenH@users.noreply.github.com>
2023-09-15 22:10:05 +0200

    better creds reading, basics for revisions

--- var.py
+++ var.py
@@ -1,5 +1,6 @@
 import os
 import re
+import json
 
 
 VALID_SCHOOLS = os.listdir(".cache")
@@ -71,4 +72,7 @@ DEFAULT_SETTINGS = {
     key: value["default"] for key, value in SETTINGS.items()
 }
 
+
+with open("creds.json", "r", encoding="utf-8") as f:
+    CREDS = json.load(f)
 
