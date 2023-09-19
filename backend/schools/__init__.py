e003ce287be881428ec043e26f3672cb386ce65c
Belissimo_T <bela.tollking@gmail.com>
2023-09-19 03:04:10 +0200

    add rudolf hildebrandt room parser

--- backend/schools/__init__.py
+++ backend/schools/__init__.py
@@ -1,6 +1,6 @@
 import typing
 
-from . import ostwald, taro
+from . import ostwald, taro, rudolf_hildebrandt
 from ..models import Teacher, Room
 
 teacher_scrapers: dict[str, typing.Callable[[], list[Teacher]]] = {
@@ -9,5 +9,6 @@ teacher_scrapers: dict[str, typing.Callable[[], list[Teacher]]] = {
 
 room_parsers: dict[str, typing.Callable[[str], Room]] = {
     "10001329": ostwald.parse_room,
-    "10453929": taro.parse_room
+    "10453929": taro.parse_room,
+    "10078734": rudolf_hildebrandt.parse_room
 }
