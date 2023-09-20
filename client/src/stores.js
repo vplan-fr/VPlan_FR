4ab248b04754cbd056fb3879f4f3436021239d66
ArtrenH <79058115+ArtrenH@users.noreply.github.com>
2023-09-20 02:01:32 +0200

    settings bug fix

--- client/src/stores.js
+++ client/src/stores.js
@@ -3,7 +3,7 @@ import { writable } from "svelte/store";
 export const title = writable("");
 export const logged_in = writable(false);
 export const current_page = writable("plan");
-export const settings = writable({});
+export const settings = writable(JSON.parse(localStorage.getItem("settings")) || {});
 export const preferences = writable({});
 export const active_modal = writable("");
 export const notifications_list = writable([]);
