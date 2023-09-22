58ce3766dd6dcfce11f9142df132159607920ed6
Belissimo_T <bela.tollking@gmail.com>
2023-09-22 23:57:09 +0200

    improve scheduled teacher, subject extraction

--- backend/plan_processor.py
+++ backend/plan_processor.py
@@ -14,7 +14,7 @@ from .stats import LessonsStatistics
 
 
 class PlanProcessor:
-    VERSION = "71"
+    VERSION = "72"
 
     def __init__(self, cache: Cache, school_number: str, *, logger: logging.Logger):
         self._logger = logger
