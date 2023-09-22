4d9ed6c33707ddb8ee321bb587be1048721ffa11
Belissimo_T <bela.tollking@gmail.com>
2023-09-22 23:51:00 +0200

    add basic lesson statistics

---
+++ backend/stats.py
@@ -0,0 +1,41 @@
+import dataclasses
+from collections import defaultdict
+
+from backend import models
+
+
+@dataclasses.dataclass
+class LessonsStatistics:
+    count: int = 0
+    cancelled: int = 0
+    changed: int = 0
+    absent_teachers: int = 0
+
+    @classmethod
+    def from_lessons(cls, lessons: models.Lessons):
+        out = LessonsStatistics()
+
+        teacher_is_absent = defaultdict(lambda: True)
+
+        for lesson in lessons:
+            if lesson.is_scheduled:
+                continue
+
+            out.count += 1
+
+            if not lesson.takes_place:
+                out.cancelled += 1
+                out.changed += 1
+
+            elif lesson.teacher_changed or lesson.room_changed or lesson.subject_changed:
+                out.changed += 1
+
+            for teacher in lesson.teachers or []:
+                teacher_is_absent[teacher] &= not lesson.takes_place
+
+        out.absent_teachers = sum(teacher_is_absent.values())
+
+        return out
+
+    def serialize(self) -> dict:
+        return dataclasses.asdict(self)
