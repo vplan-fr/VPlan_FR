e2f6b56b87a6288a8eabcf22ce5993cbec8f0d5c
Belissimo_T <bela.tollking@gmail.com>
2023-09-26 19:39:22 +0200

    fix #181

--- backend/plan_processor.py
+++ backend/plan_processor.py
@@ -14,7 +14,7 @@ from .stats import LessonsStatistics
 
 
 class PlanProcessor:
-    VERSION = "75"
+    VERSION = "76"
 
     def __init__(self, cache: Cache, school_number: str, *, logger: logging.Logger):
         self._logger = logger
