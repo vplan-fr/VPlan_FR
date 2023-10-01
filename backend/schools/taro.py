803fc9f66943a9f3c26de3de61961c6aa4fa02c7
Belissimo_T <bela.tollking@gmail.com>
2023-10-01 04:26:34 +0200

    fix imports in school parsers

--- backend/schools/taro.py
+++ backend/schools/taro.py
@@ -1,6 +1,7 @@
 import os
 
-from backend.models import Teacher, Room
+from backend.teacher import Teacher
+from backend.room import Room
 
 
 # teachers and images from: https://taroschule.de/lehrerinnen/
