e0addf528a24e69ce3a1650cd16604a3f2d5dfb1
Belissimo_T <bela.tollking@gmail.com>
2023-10-03 21:10:57 +0200

    support nobility titles

--- backend/plan_processor.py
+++ backend/plan_processor.py
@@ -15,7 +15,7 @@ from .plan_extractor import StudentsPlanExtractor, TeachersPlanExtractor
 
 
 class PlanProcessor:
-    VERSION = "93"
+    VERSION = "94"
 
     def __init__(self, cache: Cache, school_number: str, *, logger: logging.Logger):
         self._logger = logger
