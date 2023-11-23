f4e2e579e0808a7f4ffdb73e267f4e3f5168189c
Belissimo_T <bela.tollking@gmail.com>
2023-11-23 23:06:56 +0100

    improve event graphs

--- backend/_event_analysis.py
+++ backend/_event_analysis.py
@@ -20,9 +20,11 @@ def main():
     data: dict[str, list[tuple[datetime.datetime, float]]] = defaultdict(list)
 
     for event in events.iterate_events(events.PlanCrawlCycle):
-        y = (event.end_time - event.start_time).total_seconds() / 60
+        y = (event.end_time - event.start_time).total_seconds()
         x = event.start_time
         data[creds[event.school_number]].append((x, y))
+
+    fig, ax = plt.subplots(layout='constrained')
 
     for school_number, points in data.items():
         # average hourly
@@ -33,9 +35,25 @@ def main():
         #     for hour, group in itertools.groupby(points, key=lambda p: p[0].hour)
         # ]
 
-        plt.plot(*zip(*points), label=school_number)
+        # line opacity
+        ax.plot(*zip(*points), label=school_number, marker="x", linestyle="solid", alpha=0.5, linewidth=0.5,
+                 markersize=5)
 
-    plt.legend()
+    for l, ms in zip(ax.lines, itertools.cycle('o><v^1*')):
+        l.set_marker(ms)
+        # l.set_color('black')
+
+    ax.set_ylim(bottom=0, top=max(max(y for x, y in points) for points in data.values()) * 1.1)
+
+    ax.set_ylabel('Sekunden')
+    ax.set_xlabel('Datum/Uhrzeit')
+    ax2 = ax.twinx()
+    mn, mx = ax.get_ylim()
+    ax2.set_ylim(mn / 60, mx / 60)
+    ax2.set_ylabel('Minuten')
+
+    fig.legend(loc='outside right upper')
+
 
     plt.show()
 
