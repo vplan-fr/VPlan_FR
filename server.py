361d6faff315acafb59a428931f76a0cb1716b60
Belissimo_T <bela.tollking@gmail.com>
2023-11-29 21:53:42 +0100

    fix Windows Process runtime warning

--- server.py
+++ server.py
@@ -11,6 +11,7 @@ from bson import ObjectId
 
 from endpoints.authorization import authorization
 from endpoints.api import api
+import endpoints.webpush
 
 from utils import User, AddStaticFileHashFlask, get_user, send_error, update_database, meta_to_database
 
@@ -94,4 +95,5 @@ def sw() -> Response:
 
 if __name__ == "__main__":
     update_database()
+    endpoints.webpush.start_listen()
     app.run(debug=DEBUG)
