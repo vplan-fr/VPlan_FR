0e2cd88ee2f56868624bfdb6382041a45ae595bc
OfficialBrot <36410565+OfficialFreak@users.noreply.github.com>
2024-03-26 10:28:29 +0100

    warning when autofilling in auth for school

--- client/src/base_components/Button.svelte
+++ client/src/base_components/Button.svelte
@@ -4,33 +4,39 @@
     export let type = "button";
 </script>
 
-<button on:click class={$$props.class} id={$$props.id} class:small style="background: {background}" type={type}>
+<button on:click class={$$props.class} disabled={$$props.disabled} id={$$props.id} class:small style="background: {background}" type={type}>
     <slot>Button</slot>
 </button>
 
 <style lang="scss">
-    button {
-        overflow: hidden;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        border: none;
-        color: var(--text-color);
-        border-radius: 5px;
-        padding: .5em;
-        margin: 3px;
-        font-size: var(--font-size-base);
-        position: relative;
-        hyphens: auto;
+  button {
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    color: var(--text-color);
+    border-radius: 5px;
+    padding: .5em;
+    margin: 3px;
+    font-size: var(--font-size-base);
+    position: relative;
+    hyphens: auto;
+    transition: all 0.2s ease;
 
-        .material-symbols-outlined {
-            font-size: 1.3em;
-            float: right;
-            margin-left: .2em;
-        }
-
-        &.small {
-            font-size: var(--font-size-sm);
-        }
+    .material-symbols-outlined {
+      font-size: 1.3em;
+      float: right;
+      margin-left: .2em;
     }
+
+    &.small {
+      font-size: var(--font-size-sm);
+    }
+
+    &:disabled {
+      background-color: rgba(255, 255, 255, 0.4) !important;
+      color: rgba(255, 255, 255, 0.4) !important;
+    }
+  }
 </style>
