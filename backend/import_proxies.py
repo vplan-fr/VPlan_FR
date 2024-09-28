bbf6ae9821129d6e858180d0137ba1aa90f49899
Belissimo_T <bela.tollking@gmail.com>
2024-09-28 19:05:28 +0200

    make import proxies support creds

--- backend/import_proxies.py
+++ backend/import_proxies.py
@@ -1,6 +1,8 @@
 import argparse
 from pathlib import Path
 
+import aiohttp
+
 from stundenplan24_py.proxies import ProxyProvider, Proxy
 
 
@@ -13,12 +15,21 @@ def main():
     proxy_provider = ProxyProvider(Path(args.proxies_json_file))
     with open(args.proxies_txt_file, "r") as f:
         for line in f.readlines():
-            host, port = line.rsplit(":", 1)
+            host, port, *_creds = line.rsplit(":", 3)
+
+            if len(_creds) == 2:
+                creds = aiohttp.BasicAuth(_creds[0], _creds[1])
+            elif len(_creds) == 1:
+                print(f"-> Could not parse proxy {line!r}.")
+                continue
+            else:
+                creds = None
+
             port = int(port)
             if (host, port) in proxy_provider.proxies.proxies:
                 print(f"-> Proxy {host!r}:{port!r} already exists.")
                 continue
-            proxy_provider.proxies.add_proxy(Proxy(host, int(port), None))
+            proxy_provider.proxies.add_proxy(Proxy(host, int(port), creds))
 
     proxy_provider.store_proxies()
 
