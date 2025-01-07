551797696f7f2c344c968f13d869a92bfc58347f
Belissimo_T <bela.tollking@gmail.com>
2025-01-07 15:30:02 +0100

    feat: improve typography fixer
    (cherry picked from commit 10f8f3203cbba94008ea9e318edafc65cba1ab92)

--- backend/typography_fixer.py
+++ backend/typography_fixer.py
@@ -3,14 +3,16 @@ import re
 _WEEKDAY = "(?:Mo|Di|Mi|Do|Fr|Sa|So)"
 _ROMAN_NUMERAL = "(?:I|II|III|IV|V|VI|VII|VIII|IX|X)"
 _TIME = r"(?:\d{1,2}:\d{2})"
+_NUM = r"(?:\d+(?:,\d+)?)"
 
-REGEXES = {
+_REGEXES = {
     "whitespace": [
         # deduplicate whitespace
         (r"\b {1,3}\b", " "),
 
         # insert whitespace after abbreviation dot
-        (r"(?<!\S)\b(\d+\.)\b(?!\d)", r"\1 "),  # 1.Stunde
+        # (?<!\S)
+        (r"\b(\d+\.)\b(?!\d)", r"\1 "),  # 1.Stunde
         # (?<!\d)
         (rf"(\D)\b\.({_ROMAN_NUMERAL}|\d+)\b", r"\1. \2"),  # Sek.I; Jg.12
 
@@ -20,7 +22,13 @@ REGEXES = {
     ],
     "slashes": [
         (r"\b/ {1,3}\b", "/"),
-        (r"(?<=\w)/ ", "/")  # remove spaces after slashes like in G/ R/ W
+        (r"\b /\b", "/"),
+        (r"\b {1,3}/ {1,3}\b", " / "),
+
+        (r"\b(\d+\.?)/ {1,3}\b(\d+\.?)\b", r"\1/\2"),
+        (r"\b(\d+\.?) /\b(\d+\.?)\b", r"\1/\2"),
+
+        # (r"(?<=\w)/ ", "/")  # remove spaces after slashes like in G/ R/ W
     ],
     "commas": [
         # missing after comma
@@ -30,29 +38,42 @@ REGEXES = {
         # Bis-Strich
         (rf"({_WEEKDAY}) ?- ?({_WEEKDAY})", r"\1–\2"),
         (rf"({_TIME}) ?- ?({_TIME})", r"\1–\2"),
-        (rf"(?<!\.)(\d+) ?- ?(\d+)(?!\.)", r"\1–\2"),
+        (rf"(?<![\./:])(\d+) ?- ?(\d+)(?!\.|\s*[+-=*/])", r"\1–\2"),
         (rf"(\d+\.) ?- ?(\d+\.)", r"\1–\2"),
 
         # Gedankenstrich
-        (r"\b - \b", " – "),
+        (r"(\)|\b) - (-?)\b", r"\1 – \2"),
     ],
     "colons": [
         # missing whitespace after colon
-        (r"(\w+–\w+:)\b", r"\1 "),
+        (r"(?<!:)\b( +– +:)\b", r"\1 "),
 
         # whitespace erroneously before colon
-        (r"\b :\b", ": "),
+        (r"(?<!\d)\b ?: ?\b(?!\d)", ": "),
     ],
     "apostrophes": [
         (r"\b[´`']\b", "’")
     ],
     "units": [
-        (r"\b(\d+(?:,\d+)?)€", r"\1 €"),
+        (rf"\b({_NUM})€", r"\1 €"),
+    ],
+    "operators": [
+        # unbalanced whitespace around +
+        (r"\b \+\b", " + "),
+        (r"\b\+ \b", " + "),
+    ],
+    "unary-minus": [
+        (r"(?:^|\s)-(\d+)\b", r" −\1"),
+    ],
+    "operator-minus": [
+        (r"\b(\d+) ?- ?(\d+)\b", r"\1−\2"),
     ]
 }
 
+REGEXES = {key: [(re.compile(pattern), replace) for pattern, replace in ops] for key, ops in _REGEXES.items()}
 
-def cleanup(string: str):
+
+def fix_typography(string: str):
     for ops in REGEXES.values():
         for pattern, replace in ops:
             string = re.sub(pattern, replace, string.strip())
@@ -74,7 +95,7 @@ def main():
 
     def clean():
         out_text.delete("1.0", "end")
-        out_text.insert("1.0", cleanup(textedit.get("1.0", "end")))
+        out_text.insert("1.0", fix_typography(textedit.get("1.0", "end")))
 
     # run clean when text is modified
     textedit.bind("<KeyRelease>", lambda _: clean())
