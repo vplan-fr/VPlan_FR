a1d4e69a132ad405130f21e79f9ef5fedcf2cd80
Belissimo_T <bela.tollking@gmail.com>
2025-01-07 15:43:16 +0100

    feat: use typography_fixer.py in lesson_info.py

--- backend/lesson_info.py
+++ backend/lesson_info.py
@@ -11,7 +11,7 @@ from .vplan_utils import (
     parse_periods, _parse_form_pattern, ParsedForm, parsed_forms_to_str, forms_to_str,
     _loose_parse_form_pattern
 )
-from . import teacher as teacher_model, blocks
+from . import teacher as teacher_model, blocks, typography_fixer
 from . import models
 
 
@@ -404,7 +404,7 @@ class FailedToParse(SerializeMixin, ParsedLessonInfoMessage):
     def _to_text_segments(self, lesson_date: datetime.date,
                           block_config: blocks.BlockConfiguration) -> list[LessonInfoTextSegment]:
         return [
-            LessonInfoTextSegment(", ".join(self.original_messages))
+            LessonInfoTextSegment(typography_fixer.fix_typography(", ".join(self.original_messages)))
         ]
 
 
@@ -802,9 +802,7 @@ def extract_teachers(lesson: models.Lesson, classes: dict[str, models.Class], *,
 def process_additional_info(info: list[str], parsed_existing_forms: list[ParsedForm],
                             teachers: teacher_model.Teachers, date: datetime.date, rooms: set[str]
                             ) -> list[list[LessonInfoTextSegment]]:
-    info = info.copy()
-    while info and not info[-1]:
-        info.pop()
+    info = [l or "" for l in info]
 
     return [
         process_additional_info_line(line, parsed_existing_forms, teachers, date, rooms=rooms)
@@ -815,8 +813,8 @@ def process_additional_info(info: list[str], parsed_existing_forms: list[ParsedF
 def process_additional_info_line(text: str, parsed_existing_forms: list[ParsedForm],
                                  teachers: teacher_model.Teachers, date: datetime.date, rooms: set[str]
                                  ) -> list[LessonInfoTextSegment]:
-    if text is None:
-        return []
+    text = typography_fixer.fix_typography(text)
+
     # TODO: Dates, Rooms
     # remove spaces after slashes like in 5/ 3
     text = re.sub(r"\b/ {1,3}\b", "/", text.strip())
